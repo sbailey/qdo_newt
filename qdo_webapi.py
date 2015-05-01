700383aa661282dd6e5b9c6cda1e1fb44b241552
Shreyas Cholia <shreyas@gmail.com>
2015-05-01 15:03:18 -0700

    login form

--- qdo_webapi.py
+++ qdo_webapi.py
@@ -8,12 +8,11 @@ Spring 2015
 """
 
 import json
-import qdo
-from qdo import Task
+
 
 import requests
 
-from flask import Flask, escape, url_for
+from flask import Flask, escape, url_for, make_response
 import flask
 app = Flask(__name__)
 
@@ -30,7 +29,7 @@ a list of queues for that user."""
 @app.route('/<username>/')
 @app.route('/<username>/queues/')
 def qlist(username):
-    newt_session_id = flask.request.cookies.get('newt_session_id')
+    newt_session_id = flask.request.cookies.get('qdo_auth_key')
     data = dict(
         ### executable="bash -lc 'qdo list'",
         executable="python -c 'import qdo; print [q.summary() for q in qdo.qlist()]' ",
@@ -73,5 +72,34 @@ def get(username, queuename):
     r = dict(command="no direct qdo CL equivalent to get a single task and change its state to running")
     return flask.jsonify(r)
     
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+
+    if flask.request.method=='POST':
+        data = dict(
+            username = flask.request.form['username'],
+            password = flask.request.form['password']
+        )
+
+        url = "https://newt.nersc.gov/newt/auth/"
+        results = requests.post(url, data)
+        if results.status_code == 200:
+            newt_sessionid = results.json()['newt_sessionid']
+            response = make_response(results.text)
+            response.set_cookie('qdo_auth_key', newt_sessionid)
+            return response
+        else:
+            return "error", 404
+
+    else:
+        return '''
+        <form action="/login/" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        '''
+
+
 if __name__ == "__main__":
     app.run(debug=True)
