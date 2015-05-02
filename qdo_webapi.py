988d05350f69d607772ee98a4cc87340cd74c579
Stephen Bailey <stephen.code@gmail.com>
2015-05-01 17:13:59 -0700

    fill in more endpoints

--- qdo_webapi.py
+++ qdo_webapi.py
@@ -32,15 +32,19 @@ def auth():
         )
     
     url = "https://newt.nersc.gov/newt/auth/"
-    r = requests.post(url, data)
-    
-    #- rename newt_sessionid -> qdo_authkey
-    results = r.json()
-    results['qdo_authkey'] = results['newt_sessionid']
-    del results['newt_sessionid']
-    
-    return json.dumps(results)
-    
+    results = requests.post(url, data)
+    if results.status_code == 200:
+        res_obj = results.json()
+        newt_sessionid = res_obj['newt_sessionid']
+        res_obj['qdo_authkey'] = res_obj['newt_sessionid']
+        del res_obj['newt_sessionid'] 
+
+        response = make_response(json.dumps(res_obj))
+        response.set_cookie('qdo_authkey', newt_sessionid)
+        return response
+    else:
+        return "error", 404    
+        
 """
 import getpass
 import requests
@@ -58,8 +62,10 @@ newt_sessionid = results.json()['newt_sessionid']
 r = requests.get("http://127.0.0.1:5000/sjbailey/queues/", cookies={'newt_sessionid': newt_sessionid})
 """
 
-def runcmd(cmd):
-    hostname = 'hopper'
+def runcmd(cmd, hostname='hopper'):
+    """
+    Uses NEWT to run a command on the requested NERSC host
+    """
     cmdurl = "https://newt.nersc.gov/newt/command/"+hostname
     qdo_authkey = flask.request.cookies.get('qdo_authkey')
     data = dict(
@@ -67,33 +73,45 @@ def runcmd(cmd):
         loginenv='true',
     )
     results = requests.post(cmdurl, data, cookies={'newt_sessionid': qdo_authkey})
-    return results.text
+    return results.json()["output"]
 
 @app.route('/<username>/')
 @app.route('/<username>/queues/')
 def qlist(username):
-    cmd = "python -c 'import qdo; print [q.summary() for q in qdo.qlist()]' "
+    ### cmd = "python -c 'import qdo; print [q.summary() for q in qdo.qlist()]' "
+    cmd = "python -c 'import qdo; print qdo.qlist()' "
     return runcmd(cmd)
     
 @app.route('/<username>/queues/<queuename>/')
 def queue(username, queuename):
-    cmd = "python -c 'import qdo; print qdo.connect({})".format(queuename)
+    cmd = """python -c 'import qdo; print repr(qdo.connect("{}"))' """.format(queuename)
     return runcmd(cmd)
     
 @app.route('/<username>/queues/<queuename>/tasks/')
 def tasks(username, queuename):
-    r = dict(command='qdo tasks '+queuename)
-    return flask.jsonify(r)
+    cmd = """python -c 'import qdo; print qdo.connect("{}").tasks()' """.format(queuename)
+    return runcmd(cmd)
 
 @app.route('/<username>/queues/<queuename>/tasks/<taskid>', methods=['POST', 'GET'])
 def task(username, queuename, taskid):
-    return "not implemented"
-
+    #- POST: add a new task to the queue
+    if flask.request.method=='POST':
+        return "Not implemented", 501
+    elif flask.request.method == 'GET':
+        #- GET: take a look at an existing Task
+        cmd = """python -c 'import qdo; print qdo.connect("{}").tasks("{}")' """.format(queuename, taskid)
+        return runcmd(cmd)
+        
+#- Should we rename this?
 @app.route('/<username>/queues/<queuename>/get/')
 def get(username, queuename):
-    r = dict(command="no direct qdo CL equivalent to get a single task and change its state to running")
-    return flask.jsonify(r)
+    cmd = """python -c 'import qdo; print qdo.connect("{}").get()' """.format(queuename)
+    return runcmd(cmd)
     
+#-----
+#- Super basic HTML login example
+#- Your browser will then cache the authentication cookie and use it for
+#- other calls to this API.
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
 
@@ -121,12 +139,12 @@ def login():
     else:
         return '''
         <form action="/login/" method="post">
-            <p><input type=text name=username>
-            <p><input type=password name=password>
+            <p>Enter your NERSC username and password</p>
+            <p>username<input type=text name=username>
+            <p>password<input type=password name=password>
             <p><input type=submit value=Login>
         </form>
         '''
-
 
 if __name__ == "__main__":
     app.run(debug=True)
