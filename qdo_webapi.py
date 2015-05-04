3ce63c533590674179577e96c7908123da4404d5
Stephen Bailey <stephen.code@gmail.com>
2015-05-03 18:42:38 -0700

    expanding; cleanup

--- qdo_webapi.py
+++ qdo_webapi.py
@@ -10,71 +10,57 @@ Spring 2015
 import json
 import requests
 
-from flask import Flask, escape, url_for, make_response
+from flask import Flask, url_for, make_response
 import flask
 app = Flask(__name__)
 
+__version__ = "0.1"
+
 @app.route('/')
 def start():
+    """
+    Returns hello world introdution message
+    """
     r = dict()
-    r['version'] = "0.1"
+    r['version'] = __version__
     r['info'] = """\
-QDO (kew-doo) is a lightweight toolkit for processing many tasks in a queue.
+QDO (kew-doo) is a lightweight toolkit for processing many tasks in a queue,
+based upon the underlying python toolkit at https://bitbucket.org/berkeleylab/qdo .
 This is the web api for that toolkit.  Start at {baseurl}/{username} for
 a list of queues for that user."""
     return flask.jsonify(r)
 
-@app.route('/auth', methods=['POST'])
+@app.route('/auth', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def auth():
-    data = dict(
-        username = flask.request.form.get('username'),
-        password = flask.request.form.get('password'),
-        )
+    """
+    Returns results with qdo_authkey cookie and data including keys
     
-    url = "https://newt.nersc.gov/newt/auth/"
-    results = requests.post(url, data)
-    if results.status_code == 200:
-        res_obj = results.json()
-        newt_sessionid = res_obj['newt_sessionid']
-        res_obj['qdo_authkey'] = res_obj['newt_sessionid']
-        del res_obj['newt_sessionid'] 
+    """
+    if flask.request.method=='POST':
+        data = dict(
+            username = flask.request.form.get('username'),
+            password = flask.request.form.get('password'),
+            )
+    
+        url = "https://newt.nersc.gov/newt/auth/"
+        results = requests.post(url, data)
+        if results.status_code == 200:
+            res_obj = results.json()
+            newt_sessionid = res_obj['newt_sessionid']
+            res_obj['qdo_authkey'] = res_obj['newt_sessionid']
+            del res_obj['newt_sessionid'] 
 
-        response = make_response(json.dumps(res_obj))
-        response.set_cookie('qdo_authkey', newt_sessionid)
-        return response
+            response = make_response(json.dumps(res_obj))
+            response.set_cookie('qdo_authkey', newt_sessionid)
+            return response
+        else:
+            return "error", 404    
+    #- Catch other verbs to disambiguate an accidental GET auth from
+    #- getting the queues for a user named "auth", etc.
     else:
-        return "error", 404    
+        #- TODO: should include an allow header specifying POST
+        return "error", 405
         
-"""
-import getpass
-import requests
-
-#- Get newt_sessionid from NEWT authorization; cache that somehow
-url = "https://newt.nersc.gov/newt/auth/"
-data = dict()
-data['username'] = 'sjbailey'
-data['password'] = getpass.getpass()
-results = requests.post(url, data)
-newt_sessionid = results.json()['newt_sessionid']
-
-#- then use that newt_sessionid, passing in as a cookie that flask will pick
-#- up and use with its call to NEWT
-r = requests.get("http://127.0.0.1:5000/sjbailey/queues/", cookies={'newt_sessionid': newt_sessionid})
-"""
-
-def runcmd(cmd, hostname='hopper'):
-    """
-    Uses NEWT to run a command on the requested NERSC host
-    """
-    cmdurl = "https://newt.nersc.gov/newt/command/"+hostname
-    qdo_authkey = flask.request.cookies.get('qdo_authkey')
-    data = dict(
-        executable=cmd,
-        loginenv='true',
-    )
-    results = requests.post(cmdurl, data, cookies={'newt_sessionid': qdo_authkey})
-    return results.json()["output"]
-
 @app.route('/<username>/')
 @app.route('/<username>/queues/')
 def qlist(username):
@@ -145,6 +131,21 @@ def login():
             <p><input type=submit value=Login>
         </form>
         '''
+
+#- Utility function
+def runcmd(cmd, hostname='hopper'):
+    """
+    Uses NEWT to run a command on the requested NERSC host
+    """
+    cmdurl = "https://newt.nersc.gov/newt/command/"+hostname
+    qdo_authkey = flask.request.cookies.get('qdo_authkey')
+    data = dict(
+        executable=cmd,
+        loginenv='true',
+    )
+    results = requests.post(cmdurl, data, cookies={'newt_sessionid': qdo_authkey})
+    return results.json()["output"]
+
 
 if __name__ == "__main__":
     app.run(debug=True)
